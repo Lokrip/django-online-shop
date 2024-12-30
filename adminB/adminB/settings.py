5ea8df2acdaf33f5ddc687952eb9ed7287ef0ab1
Lokrip <165576212+Lokrip@users.noreply.github.com>
2024-12-30 20:22:24 +0600

    add Static

--- adminB/adminB/settings.py
+++ adminB/adminB/settings.py
@@ -236,10 +236,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
