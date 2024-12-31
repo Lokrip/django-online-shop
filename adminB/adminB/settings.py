223cd8e48dc7722831162f12059cec0c8ae00df8
Lokrip <165576212+Lokrip@users.noreply.github.com>
2024-12-31 20:25:27 +0600

    change settings

--- adminB/adminB/settings.py
+++ adminB/adminB/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["app-task1-ae9694ef0055.herokuapp.com"]
 
 
 # # Этот параметр указывает, какие источники (домен/порт) 
