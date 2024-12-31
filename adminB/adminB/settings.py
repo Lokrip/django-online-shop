b9f6d85f5788c44c06835cd31d4870ad7cd241c9
Lokrip <165576212+Lokrip@users.noreply.github.com>
2024-12-31 20:18:59 +0600

    change DB PostgreSQL on Sqlite3

--- adminB/adminB/settings.py
+++ adminB/adminB/settings.py
@@ -144,12 +144,8 @@ WSGI_APPLICATION = 'adminB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
